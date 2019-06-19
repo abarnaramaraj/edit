febdeb664d78e81b1ca4c4ad249d0fc2f8a07502
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 12:20:33 +0530

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.io.*;
+public class Mul
+{
+	public static void main (String[] args)
+{ 
+		int a,i;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+for(i=1;i<=5;i++)
+{
+System.out.print(a*i);
+}
+	}
+}
