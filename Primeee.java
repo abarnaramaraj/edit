2d8a75c2f827e910c60ff8db2c2e8996969db01a
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 12:49:42 +0530

    Create Primeee.java

---
+++ Primeee.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Primeee
+{
+	public static void main (String[] args)
+	{
+		int s1, s2,count = 0, i, j;
+		Scanner s = new Scanner(System.in);
+		s1 = s.nextInt();
+		s2 = s.nextInt();
+for(i = s1; i <= s2; i++)
+		{
+			for( j = 2; j < i; j++)
+			{
+				if(i % j == 0)
+				{
+					count = 0;
+					break;
+				}
+				else
+				{
+					count = 1;
+				}
+			}
+			if(count == 1)
+			{
+				System.out.println(i);
+			}
+		}
+ 
+	}
+}
