edecf128b51785a1e74c8dd7d83c7ad22e3aeb01
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-19 18:28:59 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Armstrong
+{
+	public static void main (String[] args) 
+	{
+		  int c=0,a,temp;  
+    int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt(); 
+    temp=n;  
+    while(n>0)  
+    {  
+    a=n%10;  
+    n=n/10;  
+    c=c+(a*a*a);  
+    }  
+    if(temp==c)  
+    System.out.println("yes");   
+    else  
+        System.out.println("no");   
+   }  
+	}
